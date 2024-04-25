3c424f7678c744e520cdce960cd4694d0340e04f
Seongah Park <97018687+pseongah@users.noreply.github.com>
2024-04-25 23:11:41 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 240425/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -5,8 +5,8 @@ even_arr = []
 for i in arr:
     if i % 2 == 0:
         even_arr.append(i)
-        
+
 rev_even = even_arr[::-1]
 
-for elem in even_arr:
+for elem in rev_even:
     print(elem, end=' ')
